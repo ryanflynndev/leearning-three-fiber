272584e2df51a01beabc49803067b79ea5c177c4
Ryan Flynn <ryanflynn44@gmail.com>
2020-12-15 13:34:12 -0500

    Added hover and active state

--- src/Box.js
+++ src/Box.js
@@ -2,3 +2,10 @@ import ReactDOM from 'react-dom'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 
+function Box(props) {
+    const mesh = useRef()
+
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    
+}
