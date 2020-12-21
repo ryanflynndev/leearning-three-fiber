b3f5f1be365ab9ab644228ae62bbf394feaaf63c
Ryan Flynn <ryanflynn44@gmail.com>
2020-12-21 10:10:11 -0500

    Added Canvas to App and ambient light

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+
+function Box(props) {
+    const mesh = useRef()
+
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    
+    useFrame(() => {
+        mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+    })
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+    </Canvas>
   );
 }
 
